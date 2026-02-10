0596a73904fc66440bba387164f4a80deed8708d
Akash Gaurav <akash.gaurav@focalyt.com>
2026-02-10 15:31:30 +0530

    remove mobile unique

--- backend/src/models/users.js
+++ backend/src/models/users.js
@@ -80,9 +80,8 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-// Indexes
+
 UserSchema.index({ email: 1 });
-UserSchema.index({ mobile: 1 });
 UserSchema.index({ role: 1 });
 
 const User = mongoose.model('User', UserSchema);
